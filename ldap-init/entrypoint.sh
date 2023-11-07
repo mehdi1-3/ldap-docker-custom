e9d4a85eb0ac8762cdb2dd9dc6d46bc2c5504e43
mehdi1-3 <mehdimosbah1999@gmail.com>
2023-11-07 13:57:01 +0100

    update

--- ldap-init/entrypoint.sh
+++ ldap-init/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 
+
     cp /ldap-init/conf/slapd.conf /etc/ldap/slapd.conf
     cp /ldap-init/conf/config.ldif  /etc/ldap/config.ldif
     cp /ldap-init/conf/schema/ia.schema /var/lib/ldap/schema/ia.schema
@@ -17,23 +18,21 @@
     rm -rf /var/lib/ldap/*
     cp -R /ldap-init/data/* /var/lib/ldap/
 
+
+
     echo "ls /var/lib/ldap/"
     ls /var/lib/ldap/
 
-   chown -R openldap:openldap /var/lib/ldap
-   chown -R openldap:openldap /var/lib/sldap
-    #ROOT_DN_CN=$(echo "${ROOT_DN}" | awk -F "," '{ print $1 }' | sed -e "s|^.*=||g")
-    #SUFFIX_DC=$(echo "${SUFFIX}" | awk -F "," '{ print $1 }' | sed -e "s|^.*=||g")
-    #cat "${LDAP_INIT}/rootdn.ldif" \
-        #| sed -e "s|ROOT_DN_CN|${ROOT_DN_CN}|g" \
-        #| sed -e "s|ROOT_DN|${ROOT_DN}|g" \
-        #| sed -e "s|SUFFIX_DC|${SUFFIX_DC}|g" \
-        #| sed -e "s|SUFFIX|${SUFFIX}|g" \
-        #| slapadd;
 
 
-systemctl enable slapd.service
-systemctl start slapd.service
+    chown -R openldap:openldap /var/lib/ldap
 
-echo "ldap status"
-systemctl status slapd.service
+
+
+    systemctl enable slapd.service
+    systemctl start slapd.service
+
+
+
+    echo "ldap status"
+    systemctl status slapd.service

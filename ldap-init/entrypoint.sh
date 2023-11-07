30d3f40b418c74b3d9092df62a8ae20aabca3219
mehdi1-3 <mehdimosbah1999@gmail.com>
2023-11-07 14:00:46 +0100

    update

--- ldap-init/entrypoint.sh
+++ ldap-init/entrypoint.sh
@@ -1,38 +1,28 @@
 #!/bin/sh
 
+    systemctl enable slapd.service
+    systemctl start slapd.service
 
 
     cp /ldap-init/conf/slapd.conf /etc/ldap/slapd.conf
     cp /ldap-init/conf/config.ldif  /etc/ldap/config.ldif
     cp /ldap-init/conf/schema/ia.schema /var/lib/ldap/schema/ia.schema
 
-
-
     chmod 777 /usr/lib/ldap/
     chmod 777 /etc/ldap/
     chmod 777 /var/lib/ldap/
-
-
 
     echo "Initializing DATA."
     rm -rf /var/lib/ldap/*
     cp -R /ldap-init/data/* /var/lib/ldap/
 
-
-
     echo "ls /var/lib/ldap/"
     ls /var/lib/ldap/
 
-
-
     chown -R openldap:openldap /var/lib/ldap
 
-
-
     systemctl enable slapd.service
-    systemctl start slapd.service
-
-
+    systemctl restart slapd.service
 
     echo "ldap status"
     systemctl status slapd.service
